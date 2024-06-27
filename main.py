5f203d6b9e44955b28e0d654b50c93a5af46f1a5
trandoanhuyphuoc19112004 <tdhphuoc22clc@fitus.edu.vn>
2024-06-27 14:47:58 +0700

    Update dfs ids dls

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import Visuallize
 from Board import Board
-from level1 import UCS
+from level1 import DFS, UCS, IDS
 
 if __name__ == "__main__":
-    #Visuallize.main() #co sys.exit() nen phai comment neu muon chay code duoi
+   # Visuallize.main() #co sys.exit() nen phai comment neu muon chay code duoi
     matrix = Visuallize.read_file("input1_level1.txt")
     board = Board(matrix)
     print(matrix)
-    path = UCS(board)
+    path, depth_limit = IDS(board)
+    
     if path:
+        print("Path found:")
         for step in path:
             print(step)
+        print(f"Depth limit: {depth_limit}")
     else:
-        print("not found")
+        print("Path not found within depth limits")
