d42165419ee551e4d3c775ff9e7bba0f2d5f79cc
RavenVelse <122483062+RavenVelse@users.noreply.github.com>
2024-07-20 15:29:57 +0700

    first menu

--- Visuallize.py
+++ Visuallize.py
@@ -3,6 +3,10 @@ import sys
 import numpy as np
 import time 
 import random
+from Button import Button
+from level1 import DFS, UCS, IDS, BFS, GBFS, Asearch
+from level3 import A_star_search,gbfs
+from level2 import Asearch2,UCS_2
 
 step_index = 0
 cell_size = 50
@@ -133,6 +137,9 @@ def highlight_path(path):
         pygame.display.flip()  # Update the display
 
 #Helper
+def get_font(size):
+    return pygame.font.Font(None, size)
+
 def calculate_total_cost(board, path):
     if path == None:
         return 0
@@ -143,8 +150,68 @@ def calculate_total_cost(board, path):
     return total_cost
 
 
-#Main function   
+#Main function
+def menu(board,path):
+    window =pygame.display.set_mode((1500,700))
+    screen_width = 1500
+    screen_height = 700
+    pygame.display.set_caption("Menu")
+    window.fill("White")
+    while True:
+        MENU_MOUSE_POS =pygame.mouse.get_pos()
+        
+        MENU_TEXT = get_font(100).render("Choose the level",True,"Black")
+        MENU_RECT = MENU_TEXT.get_rect(center=(screen_width  / 2, screen_height / 8))
+        
+        button_y_positions = [
+            screen_height / 4,         # LVL1 button
+            screen_height/ 4 + 100,   # LVL2 button
+            screen_height / 4 + 200,   # LVL3 button
+            screen_height / 4 + 300,   # LVL4 button
+            screen_height / 4 + 400    # Quit button
+        ]
+        
+        
+        LVL1_BUTTON = Button(None, pos=(screen_width / 2, button_y_positions[0]), 
+                             text_input="Level 1", font=get_font(75), base_color="Black", hovering_color="#8d8d8d")
+        LVL2_BUTTON = Button(None, pos=(screen_width / 2, button_y_positions[1]), 
+                                text_input="Level 2", font=get_font(75), base_color="Black", hovering_color="#8d8d8d")
+        LVL3_BUTTON= Button(None, pos=(screen_width / 2, button_y_positions[2]), 
+                        text_input="Level 3", font=get_font(75), base_color="Black", hovering_color="#8d8d8d")
+        LVL4_BUTTON = Button(None, pos=(screen_width / 2, button_y_positions[3]), 
+                        text_input="Level 4", font=get_font(75), base_color="Black", hovering_color="#8d8d8d")
+        QUIT_BUTTON = Button(None, pos=(screen_width / 2, button_y_positions[4]), 
+                             text_input="QUIT", font=get_font(75), base_color="Black", hovering_color="#8d8d8d")
+
+        
+        window.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [LVL1_BUTTON, LVL2_BUTTON,LVL3_BUTTON, LVL4_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(window)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if LVL1_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    start(board,path)
+                if LVL2_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    print("lvl2")
+                if LVL3_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    print("help")
+                if LVL4_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    print("credits")
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+        
 def start(board, path):
+    pygame.display.set_caption("Start")
     init_screen(board.rows,board.cols)
     #Game loop
     run = True
